ebacafd427e8018120e31001f1fb9acf9934977c
league <league@imac-18.attlocal.net>
2022-08-13 16:31:21 -0700

    I did unbirthday

--- src/_03_if_else/_1_unbirthday/Unbirthday.java
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -5,6 +5,13 @@ import javax.swing.JOptionPane;
 public class Unbirthday {
 public static void main(String[] args) {
 	String birthday = JOptionPane.showInputDialog("When is your birthday?");
+if (birthday.equals("today")) {
+	JOptionPane.showMessageDialog(null,"Happy birthday!");
+}
+else {
+	JOptionPane.showMessageDialog(null,"Happy unbirthday! ");
+}
+}
+}
 
-}
-}
+
