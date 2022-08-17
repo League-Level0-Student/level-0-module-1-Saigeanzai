b45a4adca470bfa3eb64264f20c38db4264441b6
league <league@imac-12.attlocal.net>
2022-08-17 16:56:28 -0700

    I did "if else"

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,9 +8,18 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-String
-		// 2. Ask the user to enter a name. Store their answer in a variable.
+ String jane = "she is mean";
+ String sam = "he is shy";
+ String grace = "she talks a lot";
+ String jack = "he loves basketball";
+ String emily = "she is very smart";
+ String anderson = "he loves science";
+ String maddison = "she loves her horses";
+ String jason = "he is rich";
+ String lily = "she loves plants";
 
+		// 2. Ask the user to enter a name. Store their answer in a variable.
+   JOptionPane.showInputDialog("enter a name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
 	}
