09e546b4ae8c0fca3b350f969f015ef6e8ff3a91
league <league@imac-18.attlocal.net>
2022-08-29 16:56:19 -0700

    I did riddler

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,71 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String input = JOptionPane.showInputDialog("I am an odd number. Take away a letter and I become even. What number am I?");
+
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (input.equals("7")){
+	JOptionPane.showMessageDialog(null,"correct!" );
+	score += 1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null,"wrong" );
+}
 		// 6. Add some more riddles
+  String candle = JOptionPane.showInputDialog("I'm tall when I'm young and I'm short when I'm old. What am I?");
 
+  if (candle.equals("a candle")){
+		JOptionPane.showMessageDialog(null,"correct!" );
+		score += 1;
+	}
+  else {
+	  JOptionPane.showMessageDialog(null,"wrong" );
+  }
+  String mushroom = JOptionPane.showInputDialog("What kind of room has no doors or windows?");
+  if (mushroom.equals("a mushroom")){
+		JOptionPane.showMessageDialog(null,"correct!" );
+		score += 1;
+	}
+  else {
+	  JOptionPane.showMessageDialog(null,"wrong" );
+  }
+  
+  String marry = JOptionPane.showInputDialog("He has married many women, but has never been married. Who is he?");
+  if (marry.equals("a priest")){
+		JOptionPane.showMessageDialog(null,"correct!" );
+		score += 1;
+  }
+  else {
+	  JOptionPane.showMessageDialog(null,"wrong" );
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 		// 2. Make a pop up to show the score.
-		
+  
+		JOptionPane.showMessageDialog(null, score );
+	
 	}
 }
 
