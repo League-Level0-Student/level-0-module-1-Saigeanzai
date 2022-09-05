da280e12ee6e16b0d2a2087ea0bf779d10ec2119
league <league@imac-109.attlocal.net>
2022-09-05 16:59:09 -0700

    I did rocket ship and my age

---
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -0,0 +1,13 @@
+package _05_for_loops._2_badgers;
+
+public class Badger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int badger=0;badger<12;badger++) {
+	badger+=1;
+}
+
+	}
+
+}
